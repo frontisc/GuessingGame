de1db8899865e4400acccdd4c055d796d034ac1c
Isaac Santana da Silva <zacksantana10@gmail.com>
2022-07-11 23:45:12 -0700

    mudanca de nome de arquivo html

---
+++ JS/Script.js
@@ -0,0 +1,12 @@
+window.onload = function IniciaJogo() {
+    var numeropensado = Math.round (Math.random() * 10);
+    var element = document.getElementsByClassName('resultado')
+    var chute = window.prompt(" digite seu chute");
+
+    if (chute == numeropensado)
+        document.write ("voce acertou!<br>");
+    else {
+    element.innerHTML =`voce errou, o numero pensado foi ${numeropensado} <br>`;
+        IniciaJogo();
+    }
+}
